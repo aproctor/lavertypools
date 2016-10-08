d06f07808dca778ce028beac929645ddd30923c0
Adam Proctor <adam@uken.com>
2016-10-08 14:46:12 -0400

    fixed date assumptions

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,7 +4,7 @@ class Match < ActiveRecord::Base
   belongs_to :away_team, :class_name => "Team"
 
   scope :in_range, ( lambda do |start_dt, end_dt|
-    where( :created_at => (start_dt)..(end_dt) )
+    where( :start_time => (start_dt)..(end_dt) )
   end )
 
   def long_name()
